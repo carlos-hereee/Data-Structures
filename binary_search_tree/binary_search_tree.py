3c79d35272a2036ad79cb54ee8f517681c8ab79f
ch04937 <97hernandez.c@gmail.com>
2020-01-30 13:43:27 -0800

    imports run

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,7 +1,58 @@
 # from dll_stack import Stack
 # from dll_queue import Queue
-# import sys
-# sys.path.append('../queue_and_stack')
+from doubly_linked_list import DoublyLinkedList
+import sys
+sys.path.append('../queue_and_stack')
+
+
+class Stack:
+    def __init__(self):
+        self.size = 0
+        # Why is our DLL a good choice to store our elements?
+        # self.storage = ?
+        self.storage = DoublyLinkedList()
+
+    # push adds new items to the end of the array
+    def push(self, value):
+        return self.storage.add_to_tail(value)
+
+    # pop remvoes items to the end of the array
+    def pop(self):
+        # what if no items on stack
+        if self.storage.__len__() == 0:
+            return
+        else:
+            return self.storage.remove_from_tail()
+
+    # length of list
+    def len(self):
+        return self.storage.__len__()
+
+
+class Queue:
+    def __init__(self):
+        self.size = 0
+        # Why is our DLL a good choice to store our elements?
+        # self.storage = ?
+        self.storage = DoublyLinkedList()
+
+    # should add an itemt o the back of the queue
+    def enqueue(self, value):
+        return self.storage.add_to_tail(value)
+
+    # should remove and return an item from the front of the queue
+    def dequeue(self):
+        # what if the queue is empty
+        if self.storage.__len__() == 0:
+            return
+        # else remove it from the front
+        else:
+            return self.storage.remove_from_head()
+
+    # returns the numver of items int he queue
+    def len(self):
+        # using the len method
+        return self.storage.__len__()
 
 
 class BinarySearchTree:
@@ -74,9 +125,13 @@ class BinarySearchTree:
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
+        # go left til None
+        for_each(node)
 
-    # Print the value of every node, starting with the given node,
-    # in an iterative breadth first traversal
+        # Print the value of every node, starting with the given node,
+        # in an iterative breadth first traversal
+        pass
+
     def bft_print(self, node):
         pass
 
@@ -95,4 +150,3 @@ class BinarySearchTree:
     # Print Post-order recursive DFT
     def post_order_dft(self, node):
         pass
-
