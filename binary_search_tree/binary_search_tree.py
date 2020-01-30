22b33854cff47f979dc519d0c48f653da8d024a4
ch04937 <97hernandez.c@gmail.com>
2020-01-30 12:44:38 -0800

    day 1

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,39 +1,79 @@
-import sys
-sys.path.append('../queue_and_stack')
-from dll_queue import Queue
-from dll_stack import Stack
+# from dll_stack import Stack
+# from dll_queue import Queue
+# import sys
+# sys.path.append('../queue_and_stack')
 
 
 class BinarySearchTree:
     def __init__(self, value):
+        # value would be the root
         self.value = value
         self.left = None
         self.right = None
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        # if value is less than root go left
+        if value < self.value:
+            if self.left == None:
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert(value)
+        # else it will be greater and go right
+        else:
+            if self.right == None:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        # if the value is = to the target return true
+        if target == self.value:
+            return True
+        # else if the target is greater than value go right and repeat
+        elif target > self.value:
+            if self.right == None:
+                return False
+            else:
+                return self.right.contains(target)
+        # else if the less is greater than value go right and repeat
+        elif target < self.value:
+            if self.left == None:
+                return False
+            else:
+                return self.left.contains(target)
+        # else the target is not in the tree
+        else:
+            return False
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        # loop til i get to the furthest right
+        if self.right is None:
+            # when it is none that would be the max
+            return self.value
+        else:
+            # else repeat
+            return self.right.get_max()
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
+
     def for_each(self, cb):
-        pass
+        cb(self.value)
+
+        if self.left is not None:
+            self.left.for_each(cb)
+        if self.right is not None:
+            self.right.for_each(cb)
 
     # DAY 2 Project -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
-        pass
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
@@ -55,3 +95,4 @@ class BinarySearchTree:
     # Print Post-order recursive DFT
     def post_order_dft(self, node):
         pass
+
